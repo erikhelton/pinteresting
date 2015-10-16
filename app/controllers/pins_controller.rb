a50d96091e0f92a87e71b413a39dd568a38b5aad
Erik Helton <imthinkin1@gmail.com>
2015-10-15 23:32:09 -0600

    modified aws-sdk version in Gemfile

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -18,7 +18,7 @@ class PinsController < ApplicationController
 
   def create
     @pin = current_user.pins.build(pin_params)
-    if @pin.save(pin_params)
+    if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
       render :new
